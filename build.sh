0783c6e6befad42af8e2ec5380e02c340698e22c
imide <imide@imide.xyz>
2025-05-26 14:56:05 -0600

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# totally not stolen from https://github.com/ErrorNoInternet/rpm-packages ... Thank you!!!!
+
+set -x
+
+dnf install -y git fedpkg
+git config --global --add safe.directory "$PWD"
+git checkout f42
+
+patch -p1 <../downstream.diff || {
+    echo "downstream diff failed to apply"
+    exit 1
+}
+printf "%s\n%s\n" "%global _default_patch_fuzz 2" "$(cat "$spec")" >"$spec"
+sed -i "s|\([^%]\)%{?dist}|\1.patched%{?dist}|" "$spec"
+sed -i "s|%autorelease|%autorelease -e patched|" "$spec"
+sed -i "s|-S git_am|-S git|" "$spec"
+
+fedpkg srpm
+cp ./*.src.rpm "$outdir"
